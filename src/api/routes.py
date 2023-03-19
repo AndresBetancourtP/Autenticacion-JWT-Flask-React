e1f094e29ab4a02b9f922cf919bbceeb4a85ca08
AndresBetancourtP <betancourt.andres10@gmail.com>
2023-03-19 08:36:42 +0000

    Prueba con error config

--- src/api/routes.py
+++ src/api/routes.py
@@ -9,28 +9,24 @@ from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 
+
 #Crear una aplicacion flask
 api = Blueprint('api', __name__)
 
 
-app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET')
+api.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET')
 jwt = JWTManager(app)
 
 
 # Create a route to authenticate your users and return JWTs. The
 # create_access_token() function is used to actually generate the JWT.
-@app.route("/login", methods=["POST"])
-def login():
-    username = request.json.get("username", None)
+@app.route("/token", methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
     password = request.json.get("password", None)
-    if username != "test" or password != "test":
+    if email != "test" or password != "test":
         return jsonify({"msg": "Bad username or password"}), 401
 
-    access_token = create_access_token(identity=username)
+    access_token = create_access_token(identity=email)
     return jsonify(access_token=access_token)
 
-
-
-    
-   
-    return jsonify(response_body), 200
