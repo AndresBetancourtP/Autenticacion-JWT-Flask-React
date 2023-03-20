4c63a38c4e61ea101ffcdb8e1b71b2adf647c1bb
AndresBetancourtP <betancourt.andres10@gmail.com>
2023-03-20 01:41:52 +0000

    ready step 1

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,20 +5,14 @@ import os
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
-
 
 #Crear una aplicacion flask
 api = Blueprint('api', __name__)
 
-
-api.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET')
-jwt = JWTManager(app)
-
-
-# Create a route to authenticate your users and return JWTs. The create_access_token() function is used to actually generate the JWT.
+# Create a route to authenticate your users and return JWTs. The 
+# create_access_token() function iss used to actually  generate the JWT.
 @app.route("/token", methods=["POST"])
 def create_token():
     email = request.json.get("email", None)
--- src/app.py
+++ src/app.py
@@ -10,15 +10,9 @@ from api.utils import APIException, generate_sitemap
 from api.models import db
 from api.routes import api
 from api.admin import setup_admin
-from api.commands import setup_commands
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import JWTManager
-
 #from models import Person
-
-
-app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET')
-jwt = JWTManager(app)
 
 ENV = os.getenv("FLASK_ENV")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
