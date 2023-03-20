a4df4ae3cfe18a31b4eb1742a5faaabade172559
AndresBetancourtP <betancourt.andres10@gmail.com>
2023-03-20 02:31:11 +0000

    Direccion api corregida step 1 con error

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,7 @@ import os
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 
@@ -13,7 +14,7 @@ api = Blueprint('api', __name__)
 
 # Create a route to authenticate your users and return JWTs. The 
 # create_access_token() function iss used to actually  generate the JWT.
-@app.route("/token", methods=["POST"])
+@api.route("/token", methods=["POST"])
 def create_token():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
